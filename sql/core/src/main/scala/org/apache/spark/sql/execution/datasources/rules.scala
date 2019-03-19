df87071101faabb6ed366de64caf966771c0ce66
s71955 <sujithchacko.2010@gmail.com>
2019-03-19 20:29:47 +0800

    [SPARK-26176][SQL] Verify column names for CTAS with `STORED AS`
     ## What changes were proposed in this pull request?
    Currently, users meet job abortions while creating a table using the Hive serde "STORED AS" with invalid column names. We had better prevent this by raising **AnalysisException** with a guide to use aliases instead like Paquet data source tables.
    thus making compatible with error message shown while creating Parquet/ORC native table.
    
    **BEFORE**
    ```scala
    scala> sql("set spark.sql.hive.convertMetastoreParquet=false")
    scala> sql("CREATE TABLE a STORED AS PARQUET AS SELECT 1 AS `COUNT(ID)`")
    Caused by: java.lang.IllegalArgumentException: No enum constant parquet.schema.OriginalType.col1
    ```
    
    **AFTER**
    ```scala
    scala> sql("CREATE TABLE a STORED AS PARQUET AS SELECT 1 AS `COUNT(ID)`")
     Please use alias to rename it.;eption: Attribute name "count(ID)" contains invalid character(s) among " ,;{}()\n\t=".
    ```
    
    ## How was this patch tested?
    Pass the Jenkins with the newly added test case.
    
    Closes #24075 from sujith71955/master_serde.
    
    Authored-by: s71955 <sujithchacko.2010@gmail.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>
    (cherry picked from commit e402de5fd030cdc4150fda0755c7c636cad9619e)
    Signed-off-by: colinma <colinma@tencent.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/rules.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/rules.scala
@@ -209,6 +209,8 @@ case class PreprocessTableCreation(sparkSession: SparkSession) extends Rule[Logi
         val analyzedQuery = query.get
         val normalizedTable = normalizeCatalogTable(analyzedQuery.schema, tableDesc)
 
+        DDLUtils.checkDataColNames(tableDesc.copy(schema = analyzedQuery.schema))
+
         val output = analyzedQuery.output
         val partitionAttrs = normalizedTable.partitionColumnNames.map { partCol =>
           output.find(_.name == partCol).get
@@ -222,6 +224,7 @@ case class PreprocessTableCreation(sparkSession: SparkSession) extends Rule[Logi
 
         c.copy(tableDesc = normalizedTable, query = Some(reorderedQuery))
       } else {
+        DDLUtils.checkDataColNames(tableDesc)
         val normalizedTable = normalizeCatalogTable(tableDesc.schema, tableDesc)
 
         val partitionSchema = normalizedTable.partitionColumnNames.map { partCol =>
